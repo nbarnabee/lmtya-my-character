2eccd9c49a7834ecc6242b43bba6f423e205620f
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-06-04 20:20:16 +0200

    Add new search possibilities

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 5000;
 const charList = require("./charList");
+const characters = charList.characters;
 
 app.use(express.static("public"));
 
@@ -10,7 +12,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api", (req, res) => {
-  console.log(charList.characters);
+  const responseArray = [];
+  for (let i = 0; i < characters.length; i++) {
+  for (let value in req.query) {
+    if (value === "class") {
+      if (characters[i].class.includes(req.query[value]))
+      responseArray.push(characters[i]);
+    } else if (value === "level") {
+      if (characters[i].level.reduce((a, b) => a+b) === Number(req.query.level))
+      responseArray.push(characters[i])
+    }
+    else if (characters[i][value].toLowerCase() === req.query[value].toLowerCase())
+      responseArray.push(characters[i]);
+    }
+  };
+  if (responseArray.length > 0)
+  res.json(responseArray);
+  else res.json("No matches found.")
 });
 
 app.listen(process.env.PORT || PORT, () => {
